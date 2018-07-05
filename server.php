c36b323478c86aa82e45448dca5f204d20741b56
Daniel S <danielksstone@gmail.com>
2018-07-05 16:13:43 -0700

    added missing serverphp file and danielsfile

---
+++ server.php
@@ -0,0 +1,30 @@
+<?php
+define('fromData',true);
+if(empty($_GET['action'])){
+	exit('no action specified');
+}
+require_once('mysql_connect.php');
+$output = array(
+    'success' => false,
+    'error' => [],
+    'data' => []
+);
+switch($_GET['action']) {
+    case 'readAll':
+		//include the php file 'read.php'
+		include 'dataAPI/read.php';
+        break;
+    
+    case 'insertAPI':
+        include 'dataAPI/insertAPI.php';
+        break;  
+        
+	case 'insert':
+		//include the php file insert.php read json file to insert db
+		include 'dataAPI/insert.php';
+        break;
+   
+}
+$output_json = json_encode($output);
+print($output_json);
+?>
